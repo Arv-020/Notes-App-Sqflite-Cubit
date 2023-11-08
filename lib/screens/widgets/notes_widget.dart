5c41615db16d1fac538cd5a3e72d5e1f6130b990
Arvindra <arvindra829@gmail.com>
2023-11-08 19:53:38 +0530

    dismissible widget improved

--- lib/screens/widgets/notes_widget.dart
+++ lib/screens/widgets/notes_widget.dart
@@ -51,103 +51,106 @@ class _NotesWidgetState extends State<NotesWidget> {
           )
         : Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: StaggeredGrid.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 12,
-              mainAxisSpacing: 12,
-              children: notes
-                  .map(
-                    (e) => StaggeredGridTile.fit(
-                      // axisDirection: AxisDirection.right,
-                      crossAxisCellCount: e.title.length > 100 ? 2 : 1,
-                      child: Dismissible(
-                        key: ValueKey(e.id),
-                        dragStartBehavior: DragStartBehavior.start,
-                        behavior: HitTestBehavior.deferToChild,
-                        direction: DismissDirection.up,
-                        // direction: index % 2 == 0
-                        //     ? DismissDirection.endToStart
-                        //     : DismissDirection.startToEnd,
-                        background: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(11),
-                            color: Colors.red.withOpacity(0.3),
-                          ),
-                          child: const Icon(
-                            Icons.delete,
-                            color: Colors.red,
-                          ),
-                        ),
-                        onDismissed: (direction) {
-                          context.read<AppDataBaseProvider>().removeNote(e);
-                        },
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => UpdateNoteScreen(
-                                          note: e,
-                                        )));
-                          },
-                          child: Container(
+            child: SingleChildScrollView(
+              child: StaggeredGrid.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                children: notes
+                    .map(
+                      (e) => StaggeredGridTile.fit(
+                        // axisDirection: AxisDirection.right,
+                        crossAxisCellCount: e.title.length > 100 ? 2 : 1,
+                        child: Dismissible(
+                          key: ValueKey(e.id),
+                          dragStartBehavior: DragStartBehavior.start,
+                          behavior: HitTestBehavior.deferToChild,
+                          direction: DismissDirection.horizontal,
+                          // direction: index % 2 == 0
+                          //     ? DismissDirection.endToStart
+                          //     : DismissDirection.startToEnd,
+                          background: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(11),
-                              color: Color(int.parse(e.noteColor!)),
+                              color: Colors.red.withOpacity(0.3),
                             ),
-                            // margin: EdgeInsets.only(
-                            //     left: index % 2 == 0 ? 20 : 0,
-                            //     right: index % 2 == 1 ? 20 : 0),
-                            // height: MediaQuery.sizeOf(context).height * 0.3,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              // mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 20.0, left: 20, right: 20),
-                                  child: Text(
-                                    e.title,
-                                    style: GoogleFonts.mPlus1(
-                                        fontSize:
-                                            e.title.length > 100 ? 22 : 18,
-                                        fontWeight: e.title.length > 100
-                                            ? FontWeight.w700
-                                            : FontWeight.w500),
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                          ),
+                          onDismissed: (direction) {
+                            context.read<AppDataBaseProvider>().removeNote(e);
+                          },
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => UpdateNoteScreen(
+                                            note: e,
+                                          )));
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(11),
+                                color: Color(int.parse(e.noteColor!)),
+                              ),
+                              // margin: EdgeInsets.only(
+                              //     left: index % 2 == 0 ? 20 : 0,
+                              //     right: index % 2 == 1 ? 20 : 0),
+                              // height: MediaQuery.sizeOf(context).height * 0.3,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                // mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 20.0, left: 20, right: 20),
+                                    child: Text(
+                                      e.title,
+                                      style: GoogleFonts.mPlus1(
+                                          fontSize:
+                                              e.title.length > 100 ? 22 : 18,
+                                          fontWeight: e.title.length > 100
+                                              ? FontWeight.w700
+                                              : FontWeight.w500),
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 20, right: 20, bottom: 20),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    mainAxisAlignment: e.title.length > 100
-                                        ? MainAxisAlignment.end
-                                        : MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        e.time,
-                                        style: GoogleFonts.mPlus1(
-                                          fontSize: 16,
-                                          color: Colors.grey.shade800
-                                              .withOpacity(1),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 20, bottom: 20),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      mainAxisAlignment: e.title.length > 100
+                                          ? MainAxisAlignment.end
+                                          : MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          e.time,
+                                          style: GoogleFonts.mPlus1(
+                                            fontSize: 16,
+                                            color: Colors.grey.shade800
+                                                .withOpacity(1),
+                                          ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                  .toList(),
+                    )
+                    .toList(),
+              ),
             ));
   }
 }
